a542144fdf9fb2731e447aa114c41216095abae4
Nathan Pearson <nate.m.pearson@gmail.com>
2018-09-05 17:34:53 -0400

    snakeCase solution

--- snakeCase/snakeCase.js
+++ snakeCase/snakeCase.js
@@ -1,5 +1,9 @@
-const snakeCase = function() {
-
+const snakeCase = function(str) {
+    let newStr = str.replace(/[^A-z]/g, ' ');
+    if(newStr.indexOf(' ') < 0) {
+        newStr = newStr.replace(/([A-Z])/g, ' $1');    
+    }
+    return newStr.trim().toLowerCase().replace(/\s+/g, '_');
 }
 
 module.exports = snakeCase
