5b58029d2865d9328ded01511e79091af1ccc33d
Nathan Pearson <nate.m.pearson@gmail.com>
2018-09-05 17:33:00 -0400

    pigLatin solution

--- pig_latin/pigLatin.js
+++ pig_latin/pigLatin.js
@@ -1,6 +1,23 @@
-function translate() {
-	// body...
+function translate(str) {
+	return str
+    .split(" ")
+    .map(word => {
+      const index = firstVowelIndex(word);
+      const beginning = word.slice(0, index);
+      const ending = word.slice(index);
+      return `${ending}${beginning}ay`;
+    })
+    .join(" ");
 }
+
+function firstVowelIndex(str) {
+	const vowels = str.match(/[aeiou]/g);
+	if(vowels[0] == "u" && str[str.indexOf(vowels[0]) - 1] == "q") {
+		return str.indexOf(vowels[1]);
+	}
+	return str.indexOf(vowels[0]);
+}
+
 
 
 module.exports = {
