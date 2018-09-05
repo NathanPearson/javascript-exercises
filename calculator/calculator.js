d88494387af07a5afb5778f5bafd45e82128a3c7
Nathan Pearson <nate.m.pearson@gmail.com>
2018-09-05 17:31:19 -0400

    calculator solution

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,37 @@
-function add () {
-	
+function add (a, b) {
+	return a + b;
 }
 
-function subtract () {
-	
+function subtract (a, b) {
+	return a - b;
 }
 
-function sum () {
-	
+function sum (a) {
+	if (!Array.isArray(a) || !a.length)
+		return 0;
+	return a.reduce(add);
 }
 
-function multiply () {
-	
+function multiply (a) {
+	return a.reduce((a,b)=> a*b);
 }
 
-function power() {
-	
+function power(a, b) {
+	let res = 0;
+	for(i = 0; i <= b; i++) {
+		res += a * a;
+	}
+	return res;
 }
 
-function factorial() {
-	
+function factorial(a) {
+	if(a < 0)
+		return -1;
+	else if (a == 0) 
+		return 1;
+	else { 
+		return(a * factorial(a - 1));
+	}	
 }
 
 module.exports = {
