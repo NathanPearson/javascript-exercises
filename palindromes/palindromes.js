f7db03dc72347878966b2d9e212bcb16d5fc32cf
Nathan Pearson <nate.m.pearson@gmail.com>
2018-09-05 17:32:39 -0400

    palindromes solution

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function() {
-
+const palindromes = function(str) {
+    var reg = /[\W]+/g;
+    str = str.toLowerCase().replace(reg, '');    
+    j = str.length - 1;    
+    for(i=0;i <= j;i++){    
+        if(str.charAt(i) != str.charAt(j)) return false;
+        --j;
+    }
+    return true;
 }
 
 module.exports = palindromes
