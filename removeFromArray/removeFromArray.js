337ef6c3dce6a5cb97c49b7c775f677ba92ae0f6
Nathan Pearson <nate.m.pearson@gmail.com>
2018-09-05 17:33:25 -0400

    removeFromArray solution

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
 const removeFromArray = function() {
+    let args = Array.from(arguments);
+    let res = args.shift();
+        
+    res = res.filter(item => !args.includes(item));
 
+    return res;
 }
 
 module.exports = removeFromArray
