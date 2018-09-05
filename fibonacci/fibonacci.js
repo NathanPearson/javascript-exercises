1bf0caa35836ec5b8d48ab994925ff7541e2f60a
Nathan Pearson <nate.m.pearson@gmail.com>
2018-09-05 17:31:36 -0400

    fibonacci solution

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let a = 1; b = 1; temp = 0;
+    num = parseInt(num, 10);
+    if (num < 1)
+        return 'OOPS';
+    if (num < 2)
+        return 1;
+    for (i=0; i < num - 2; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+    }
 
+    return b;
 }
 
 module.exports = fibonacci
