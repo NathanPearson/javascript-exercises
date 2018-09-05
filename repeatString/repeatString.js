7a930e867779abf8e2e97d11a4e6e740c71f1dc2
Nathan Pearson <nate.m.pearson@gmail.com>
2018-09-05 17:33:42 -0400

    repeatString solution

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    if(times < 0) {
+        return 'ERROR';
+    }
+    let res = '';
+    for(i=0;i<times;i++) {
+        res += str;
+    }
+    return res;
 }
 
 module.exports = repeatString
