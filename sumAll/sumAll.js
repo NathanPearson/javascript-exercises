91857eaf5ad8b42b260518aed2c47bead820ebff
Nathan Pearson <nate.m.pearson@gmail.com>
2018-09-05 17:35:08 -0400

    sumAll solution

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if(typeof a !== 'number' || typeof b !== 'number') {
+        return 'ERROR';
+    } else if (a < 0 || b < 0) {
+        return 'ERROR';
+    } else {
+        let sum = 0;
+        let temp;
 
+        if(a > b) {
+            temp = b;
+            b = a;
+            a = temp;
+        }
+        while (a <= b) {
+            sum += a;
+            a++;
+        }
+        return sum;
+    }
 }
 
 module.exports = sumAll
